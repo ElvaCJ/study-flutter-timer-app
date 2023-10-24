68bcfbb46e2f24bafde3a905d409aa042ea5e321
Chanjoo Park <cjangel0503@gmail.com>
2023-10-25 00:32:22 +0900

    feat: Implement Methods Needed to Function a Timer

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,150 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const timeISet = 1500;
+  int totalSeconds = timeISet;
+  bool isRunning = false;
+  int totalPomodoros = 0;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros++;
+        isRunning = false;
+        totalSeconds = timeISet;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds--;
+      });
+    }
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  String format(int sec) {
+    var duration = Duration(seconds: sec);
+    return duration.toString().split('.').first.substring(2, 7);
+  }
+
+  void restartPressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+      totalSeconds = timeISet;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                format(totalSeconds),
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 88,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  iconSize: 120,
+                  color: Theme.of(context).cardColor,
+                  icon: Icon(isRunning
+                      ? Icons.pause_circle_outline_rounded
+                      : Icons.play_circle_outline_rounded),
+                  // 타이머가 작동중일 때(isRunning==true) 버튼을 누르면(onPressed) onPausePressed 실행되며 일시정지.
+                  onPressed: isRunning ? onPausePressed : onStartPressed,
+                ),
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  iconSize: 95,
+                  color: Theme.of(context).cardColor,
+                  icon: const Icon(Icons.stop_circle_rounded),
+                  onPressed: restartPressed,
+                ),
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodoros',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
+                          ),
+                        ),
+                        Text(
+                          '$totalPomodoros',
+                          style: TextStyle(
+                            fontSize: 55,
+                            fontWeight: FontWeight.w600,
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
